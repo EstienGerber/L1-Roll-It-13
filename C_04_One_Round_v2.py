f74f62fd3d484fa4e8d71847f983247356357b52
EstienGerber <estien.gerber@papamoacollege.school.nz>
2025-04-10 12:09:31 +1200

    Add files via upload

--- C_04_One_Round_v2.py
+++ C_04_One_Round_v2.py
@@ -19,14 +19,20 @@ def initial_points(which_player):
     return total, double
 
 
-# Roll the dice fpr the user and note if they got a double
-initial_user = initial_points("User")
-initial_comp = initial_points("Comp")
+def make_statement(statement, decoration):
+    """Adds emoji / additional characters to the start and end of the headings"""
+
+    ends = decoration * 3
+    print(f"\n{ends} {statement} {ends}")
 
 # Main starts here...
 
 
-# Retrive user points (first item returned from function)
+# Roll the dice fpr the user and note if they got a double
+initial_user = initial_points("User")
+initial_comp = initial_points("Comp")
+
+# Retrieve user points (first item returned from function)
 user_points = initial_user[0]
 comp_points = initial_comp[0]
 
@@ -85,7 +91,7 @@ user_points = player_1_points
 comp_points = player_2_points
 
 # switch the user and computer points if the computer went first
-if first == "Commputer":
+if first == "Computer":
     user_points, comp_points = comp_points, user_points
 
 # work out who won...
@@ -101,9 +107,7 @@ if winner == "user" and double_user == "yes":
     user_points = user_points * 2
 
 # Output round results
-print("\nRound Results")
+make_statement("Round Results", "=")
 print(f"User Points: {user_points} | Computer Points: {comp_points}")
 print(round_feedback)
-print()score is updated)
-    player_2_roll = random.randint(1, 6)
-    player_2_points += player_2_roll
+print()
